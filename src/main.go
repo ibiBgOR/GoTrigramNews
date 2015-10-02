79ce1b7b2a9f1cb969215704f51555e46d2f263b
Oscar Ruckdeschel <plasmaosc@aol.com>
2015-10-02 18:56:32 +0200

    Improved n-gramm func to not run into an outOfRangeError

--- src/main.go
+++ src/main.go
@@ -15,16 +15,8 @@ type trigram struct {
 func buildNGram(sequence string, n int) []trigram {
 	var result []trigram
 
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Print(result)
-		}
-	}()
-
-	for i := 0; i < len(sequence); i += 1 {
+	for i := 0; i + 2 < len(sequence); i += 1 {
 		result = append(result, trigram{string(sequence[i]), string(sequence[i + 1]), string(sequence[i + 2])})
 	}
-
-	fmt.Print(result)
 	return result
 }
